0a63b972cf3a893360a85d11179418615cd3e5f3
abdullah3422 <22953@students.riphah.edu.pk>
2024-03-08 11:24:24 +0500

    OwnerAnalytics Completed +  Added comments to styling, Added new Icons

--- src/helpers/ImageImports.js
+++ src/helpers/ImageImports.js
@@ -4,4 +4,8 @@ export const icons = {
   calendarIcon: require("../assets/icons/calendar.png"),
   emailIcon: require("../assets/icons/email.png"),
   phoneNumberIcon: require("../assets/icons/phone-number.png"),
+  downArrowGreen: require("../assets/icons/down-arrowGreen.png"),
+  upArrowRed: require("../assets/icons/up-arrowRed.png"),
+  incomingArrow: require("../assets/icons/incoming-Arrow.png"),
+  outgoingArrow: require("../assets/icons/outgoing-Arrow.png"),
 };
