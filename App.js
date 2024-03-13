2bf5ad356ad88c3b867e1ad97b1b72a1dedbd731
abdullah3422 <22953@students.riphah.edu.pk>
2024-03-13 18:06:57 +0500

    OwnerProperty menu added to App.js

--- App.js
+++ App.js
@@ -43,7 +43,6 @@ export default function App() {
           screenOptions={{
             headerShown: false,
           }}
-          initialRouteName="Owner Navigator"
           initialRouteName="Owner Property Menu"
         >
           {/* Authentication Screens */}
