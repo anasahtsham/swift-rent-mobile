1419d98dad0be35f55cc80aa8c51d40d3e00eac2
Siamax <ibrahimahtsham2002@gmail.com>
2024-05-03 21:43:40 +0500

    ban check implemented on splash screen

--- src/splash/SplashScreen.js
+++ src/splash/SplashScreen.js
@@ -1,57 +1,100 @@
 import { CommonActions } from "@react-navigation/native";
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, Alert, StyleSheet, Text, View } from "react-native";
 import * as FontSizes from "../assets/fonts/FontSizes";
 import SwiftRentLogoLarge from "../components/common/images/SwiftRentLogoLarge";
+import { saveUserID, saveUserType } from "../helpers";
 import { useColors } from "../helpers/SetColors";
+import { BASE_URL } from "./../constants/index";
 import { useUserID } from "./../helpers/SetUserID";
 import { useUserType } from "./../helpers/SetUserType";
 
 const SplashScreen = ({ navigation }) => {
   //set theme
   const colors = useColors();
+  const [loading, setLoading] = useState(true);
 
   const userID = useUserID();
   const userType = useUserType();
 
   // timer to send off from splash screen
   useEffect(() => {
+    console.log("userID: ", userID);
+    console.log("userType: ", userType);
+    if (userID && userType) {
+      axios
+        .post(`${BASE_URL}/api/auth/check-ban`, { userID: userID })
+        .then((response) => {
+          if (response.data.isBanned === true) {
+            saveUserID("");
+            saveUserType("");
+            Alert.alert(
+              "User Banned",
+              "You have been banned from the system. Please contact the admin at swiftrent2023@gmail.com for more information."
+            );
+
+            navigation.dispatch(
+              CommonActions.reset({
+                index: 0,
+                routes: [{ name: "Welcome Screen" }],
+              })
+            );
+          } else {
+            switch (userType) {
+              case "O":
+                console.log("Owner");
+                navigation.dispatch(
+                  CommonActions.reset({
+                    index: 0,
+                    routes: [{ name: "Owner Navigator" }],
+                  })
+                );
+                break;
+              case "M":
+                navigation.dispatch(
+                  CommonActions.reset({
+                    index: 0,
+                    routes: [{ name: "Manager Navigator" }],
+                  })
+                );
+                break;
+              case "T":
+                navigation.dispatch(
+                  CommonActions.reset({
+                    index: 0,
+                    routes: [{ name: "Tenant Navigator" }],
+                  })
+                );
+                break;
+              default:
+                navigation.dispatch(
+                  CommonActions.reset({
+                    index: 0,
+                    routes: [{ name: "Welcome Screen" }],
+                  })
+                );
+                break;
+            }
+          }
+        })
+        .catch((error) => {
+          // error.response prettify JSON log
+          console.log(JSON.stringify(error.response, null, 2));
+          Alert.alert("Error", error.response.data.error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
     const timer = setTimeout(() => {
-      switch (userType) {
-        case "O":
-          navigation.dispatch(
-            CommonActions.reset({
-              index: 0,
-              routes: [{ name: "Owner Navigator" }],
-            })
-          );
-          break;
-        case "M":
-          navigation.dispatch(
-            CommonActions.reset({
-              index: 0,
-              routes: [{ name: "Manager Navigator" }],
-            })
-          );
-          break;
-        case "T":
-          navigation.dispatch(
-            CommonActions.reset({
-              index: 0,
-              routes: [{ name: "Tenant Navigator" }],
-            })
-          );
-          break;
-        default:
-          navigation.dispatch(
-            CommonActions.reset({
-              index: 0,
-              routes: [{ name: "Welcome Screen" }],
-            })
-          );
-          break;
-      }
-    }, 3000); // where 1000 milliseconds = 1 second
+      navigation.dispatch(
+        CommonActions.reset({
+          index: 0,
+          routes: [{ name: "Welcome Screen" }],
+        })
+      );
+    }, 10000); // where 1000 milliseconds = 1 second
 
     // Clear the timer to prevent memory leaks
     return () => clearTimeout(timer);
@@ -63,7 +106,7 @@ const SplashScreen = ({ navigation }) => {
     >
       <SwiftRentLogoLarge />
 
-      <View style={styles.textContainer}>
+      <View style={[styles.textContainer, { marginBottom: 30 }]}>
         <Text
           style={[
             styles.splashTextSwift,
@@ -81,6 +124,9 @@ const SplashScreen = ({ navigation }) => {
           Rent
         </Text>
       </View>
+      {loading && (
+        <ActivityIndicator size="large" color={colors.textDarkBlue} />
+      )}
     </View>
   );
 };
